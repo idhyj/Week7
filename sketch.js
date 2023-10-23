fce4bbdf793e103011faf0f3ff51c930737e5172
idhyj <145718308+idhyj@users.noreply.github.com>
2023-10-23 15:45:27 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-let cannonAngle = 90; // 총알대의 초기 각도 (90도는 위를 가리킴)
-let bulletX, bulletY; // 총알의 초기 위치
-let targetX, targetY; // 동그라미의 위치
-let score = 0; // 획득한 점수
-let gameEnded = false; // 게임 종료 여부
-let successSound, hitSound; // 성공 효과음
+let cannonAngle = 90; // Initial angle of the cannon (90 degrees points upwards)
+let bulletX, bulletY; // Initial position of the bullet
+let targetX, targetY; // Position of the target circle
+let score = 0; // Points obtained
+let gameEnded = false; // Game end flag
+let successSound, hitSound; // Success and hit sound effects
 
 function preload() {
-  successSound = loadSound('yeah.mp3'); // 성공 효과음 로드
-  hitSound = loadSound('puuu.mp3'); // 적중 효과음 로드
+  successSound = loadSound('yeah.mp3'); // Load success sound effect
+  hitSound = loadSound('puuu.mp3'); // Load hit sound effect
 }
 
 function setup() {
@@ -22,15 +22,15 @@ function draw() {
   background(0);
 
   if (!gameEnded) {
-    // 게임이 종료되지 않은 경우에만 총알대를 움직일 수 있음
-    handleInput(); // 키보드 입력 처리
+    // Can only move the cannon if the game is not over
+    handleInput(); // Handle keyboard input
   }
 
-  // 동그라미 그리기
+  // Draw the target circle
   fill(255, 0, 0);
   ellipse(targetX, targetY, 30, 30);
 
-  // 총알대 그리기
+  // Draw the cannon
   push();
   translate(bulletX, bulletY);
   rotate(radians(cannonAngle));
@@ -38,46 +38,46 @@ function draw() {
   rect(-10, -5, 20, 10);
   pop();
 
-  // 총알 이동
+  // Move the bullet
   if (!gameEnded) {
     bulletX += 5 * cos(radians(cannonAngle));
     bulletY -= 5 * sin(radians(cannonAngle));
   }
 
-  // 총알이 캔버스 밖으로 나가면 리셋
+  // Reset the bullet if it goes outside the canvas
   if (bulletY < 0 || bulletX < 0 || bulletX > width) {
     bulletY = height;
     bulletX = width / 2;
   }
 
-  // 동그라미를 맞춘 경우
+  // If the bullet hits the target circle
   let distance = dist(bulletX, bulletY, targetX, targetY);
   if (distance < 15) {
     score++;
     targetX = random(width);
     targetY = random(50, height - 50);
-    // 5점 달성 시 성공 효과음 재생
+    // Play success sound effect when reaching 5 points
     if (score >= 5) {
       successSound.play();
     }
-    // 맞춘 경우 적중 효과음 재생
+    // Play hit sound effect when hitting the target
     hitSound.play();
   }
 
-  // 점수 표시
+  // Display the score
   fill(255);
   textSize(24);
   textAlign(RIGHT);
   text("Score: " + score, width - 20, 30);
 
-  // 성공 메시지 표시
+  // Display success message
   if (score >= 5) {
     gameEnded = true;
     fill(0, 255, 0);
     textSize(48);
     textAlign(CENTER, CENTER);
     text("Success!", width / 2, height / 2);
-    // 처음으로 돌아가기 버튼 표시
+    // Display the restart button
     textAlign(CENTER);
     textSize(24);
     fill(255);
@@ -86,24 +86,24 @@ function draw() {
 }
 
 function keyPressed() {
-  // 오른쪽 화살표 키를 누르면 총알대를 왼쪽으로 회전
+  // Rotate the cannon to the left when the right arrow key is pressed
   if (keyCode === RIGHT_ARROW) {
     cannonAngle -= 5;
   }
-  // 왼쪽 화살표 키를 누르면 총알대를 오른쪽으로 회전
+  // Rotate the cannon to the right when the left arrow key is pressed
   else if (keyCode === LEFT_ARROW) {
     cannonAngle += 5;
   }
 }
 
 function handleInput() {
-  // 키보드 입력을 처리하는 함수
-  // 여기에서 추가적인 입력 로직을 추가할 수 있습니다.
+  // Function to handle keyboard input
+  // Additional input logic can be added here
 }
 
 function mousePressed() {
   if (gameEnded) {
-    // 게임이 종료된 경우 마우스를 클릭하면 게임 재시작
+    // Restart the game when the mouse is clicked after the game is over
     score = 0;
     gameEnded = false;
     bulletY = height;
